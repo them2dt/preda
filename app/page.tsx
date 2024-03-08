65349aa5a544ceea16a9bdedd81f895f6a19ef64
maruthan4r <mtcreatives@hotmail.com>
2024-03-08 16:19:01 +0100

    add lab-router

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 //panels
 import CreateNFTPanel from "../panels/nft/Create";
@@ -12,20 +13,56 @@ export default function Home() {
   const [theme, setTheme] = useState(0);
   const [connected, setConnected] = useState(false);
   const [panel, setPanel] = useState(0);
+  const [operation, setOperation] = useState(0);
+  const [operationDrop, setOperationDrop] = useState(false);
+  const [type, setType] = useState(0);
+  const [typeDrop, setTypeDrop] = useState(true);
+
+  const operationLabels = ["create", "edit", "burn"];
+  const typeLabels = [
+    "NFT",
+    "pNFT",
+    "cNFT",
+    "SPL-20",
+    "Token2022",
+    "Token extension",
+  ];
+
   return (
-    <div
+    <motion.div
       id="skeleton"
       className="skeleton"
       data-theme={theme == 0 ? "light" : "dark"}
     >
       <Header panel={panel} setPanel={setPanel} theme={theme} />
-      <div id="mainboard" className="flex-column-center-center">
-        <CreateNFTPanel />
+      <div id="mainboard" className="flex-row-center-center">
+        <div className="flex-row-center-start">
+          <motion.div className="type-selector flex-column-center-start">
+            <motion.div className="type-options flex-column-center-start">
+              {typeLabels.map((label, index) => {
+                return (
+                  <motion.div
+                    className={
+                      type == index ? "type-option active" : "type-option"
+                    }
+                    onClick={() => {
+                      setType(index);
+                    }}
+                  >
+                    {label}
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          </motion.div>
+
+          <CreateNFTPanel />
+        </div>
       </div>
       <Navigation
         theme={theme}
         toggleTheme={() => setTheme(theme == 0 ? 1 : 0)}
       />
-    </div>
+    </motion.div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,8 @@ export default function Header({
   setPanel: Function;
   theme: number;
 }) {
-  const labels = ["Create NFT", "Duplicate NFT", "Edit NFT", "Burn NFT"];
-  const [operator, setOperator] = useState(false);
+  const labels = ["Lab", "Gallery"];
+  const [page, setPage] = useState(0);
 
   return (
     <>
@@ -24,81 +24,16 @@ export default function Header({
         <m.div
           className="operator font-text-tiny"
           onClick={() => {
-            setOperator(!operator);
+            if (page == 0) {
+              setPage(1);
+            } else {
+              setPage(0);
+            }
           }}
         >
-          {labels[panel]}
+          {labels[page]}
         </m.div>
       </m.div>
-      {operator && (
-        <m.div
-          id="operator-catalog"
-          className="flex-row-between-start"
-          data-theme={theme == 0 ? "light" : "dark"}
-        >
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              NFT
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              pNFT
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              cNFT
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              SPL-20
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              Token2022
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              Extensions
-            </m.div>
-            <m.div className="header-operation">Create</m.div>
-            <m.div className="header-operation">Duplicate</m.div>
-            <m.div className="header-operation">Edit</m.div>
-            <m.div className="header-operation">Burn</m.div>
-          </m.div>
-          <m.div className="header-operations-topic flex-column-center-end">
-            <m.div className="header-operations-topic-title font-text-bold">
-              General
-            </m.div>
-            <m.div className="header-operation">Gallery</m.div>
-            <m.div className="header-operation">Analytics</m.div>
-          </m.div>
-        </m.div>
-      )}
     </>
   );
 }
