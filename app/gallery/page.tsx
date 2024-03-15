1a29c9b793c8f4d24e0fead17853c128716d98b0
maruthan4r <mtcreatives@hotmail.com>
2024-03-15 11:02:24 +0100

    add gallery with loading function

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,14 +6,15 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCubes,
+  faCopy,
   faEllipsis,
-  faListDots,
-  faMinus,
-  faPlus,
+  faRotateLeft,
+  faFireFlameCurved,
 } from "@fortawesome/free-solid-svg-icons";
 import { loadNFTs } from "@/backend/General";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { enqueueSnackbar } from "notistack";
+import { Tooltip } from "@mui/material";
 
 export default function Home() {
   const walletParent = useWallet();
@@ -23,14 +24,66 @@ export default function Home() {
   const [theme, setTheme] = useState(0);
 
   const [nftItems, setnftItems] = useState<
-    { imageUri: String; name: string; supply: number; pubkey: string }[]
+    {
+      name: string;
+      mint: string;
+      imageUri: string;
+      updateAuthority: string;
+      attributes: { trait_type: string; value: string }[];
+      tokenStandard: string;
+    }[]
   >([]);
 
-  useEffect(() => {
+  async function loadAssets() {
     if (wallet) {
-      loadNFTs({ wallet: wallet, endpoint: "https://api.devnet.solana.com" });
+      setTimeout(() => {
+        console.log("waiting...");
+      }, 500);
+      if (wallet.adapter.connected) {
+        if (wallet.adapter.publicKey) {
+          const result = await loadNFTs({
+            wallet: wallet.adapter.publicKey.toBase58(),
+            endpoint: connection.rpcEndpoint,
+          })
+            .then((response) => setnftItems(response))
+            .catch((err) => enqueueSnackbar(err, { variant: "error" }));
+        } else {
+          enqueueSnackbar("Pubkey not found", { variant: "error" });
+        }
+      } else {
+        enqueueSnackbar("Wallet not connected", { variant: "error" });
+      }
+    } else {
+      enqueueSnackbar("Wallet not found", { variant: "error" });
     }
+  }
+  useEffect(() => {
+    async function loadAssets() {
+      if (wallet) {
+        setTimeout(() => {
+          console.log("waiting...");
+        }, 500);
+        if (wallet.adapter.connected) {
+          if (wallet.adapter.publicKey) {
+            const result = await loadNFTs({
+              wallet: wallet.adapter.publicKey.toBase58(),
+              endpoint: connection.rpcEndpoint,
+            });
+            setnftItems(result);
+          } else {
+            enqueueSnackbar("Pubkey not found", { variant: "error" });
+          }
+        } else {
+          enqueueSnackbar("Wallet not connected", { variant: "error" });
+        }
+      } else {
+        enqueueSnackbar("Wallet not found", { variant: "error" });
+      }
+    }
+
+    loadAssets().catch(console.error);
   }, [walletParent.connected]);
+
   return (
     <motion.div
       id="skeleton"
@@ -55,31 +108,49 @@ export default function Home() {
             <div className="view-type flex-row-center-center font-text">
               List
             </div>
+            <button
+              className="view-type flex-row-center-center font-text"
+              onClick={loadAssets}
+            >
+              <FontAwesomeIcon icon={faRotateLeft} />
+            </button>
           </div>
         </div>
-        <div className="gallery-container flex-row-center-start">
+        <div className="gallery-container flex-row-start-start">
           {nftItems.map((item, index) => (
             <div className="gallery-item flex-column-center-center" key={index}>
-              <div className="gallery-image"></div>
+              <div className="gallery-image">
+                <img src={item.imageUri} alt="couldn't find the image" />
+              </div>
               <div className="gallery-info">
                 <div className="gallery-title font-text-small-bold">
                   {item.name}
                 </div>
                 <div className="gallery-supply font-text-small">
-                  {item.supply}
+                  {item.mint[0] +
+                    item.mint[1] +
+                    item.mint[2] +
+                    "..." +
+                    item.mint[item.mint.length - 3] +
+                    item.mint[item.mint.length - 2] +
+                    item.mint[item.mint.length - 1]}
                 </div>
               </div>
               <div className="gallery-operations first flex-row-between-center">
-                <div className="gallery-operation flex-row-center-center">
-                  <FontAwesomeIcon icon={faPlus} />
-                </div>
+                <Tooltip title="Duplicate" arrow>
+                  <div className="gallery-operation flex-row-center-center">
+                    <FontAwesomeIcon icon={faCopy} />
+                  </div>
+                </Tooltip>
                 <div className="gallery-operation flex-row-center-center">
                   <FontAwesomeIcon icon={faEllipsis} />
                 </div>
 
-                <div className="gallery-operation first flex-row-center-center">
-                  <FontAwesomeIcon icon={faMinus} />
-                </div>
+                <Tooltip title="Burn" arrow>
+                  <div className="gallery-operation flex-row-center-center">
+                    <FontAwesomeIcon icon={faFireFlameCurved} />
+                  </div>
+                </Tooltip>
               </div>
             </div>
           ))}
--- backend/General.tsx
+++ backend/General.tsx
@@ -116,23 +116,71 @@ export async function uploadFileToIrys({
   return "https://arweave.net/" + upload.id;
 } //function which takes a file and uploads it to the arweave network using the irys-sdk
 
+async function getAsset(url: string, pubkeys: string[]) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "my-id",
+      method: "getAssetBatch",
+      params: {
+        ids: pubkeys,
+      },
+    }),
+  });
+  const { result } = await response.json();
+  console.log("Assets: ", result);
+}
 export async function loadNFTs({
   wallet,
   endpoint,
 }: {
-  wallet: Wallet;
-  endpoint: string;
-}): Promise<any> {
-  // { imageUri: String; name: string; supply: number; pubkey: string }[]
-  const umi = createUmi(endpoint).use(dasApi());
-  if (wallet.adapter.publicKey) {
-    const owner = publicKey("wallet.adapter.publicKey.toBase58()");
+  wallet: string;
+  endpoint: string; 
+}): Promise<
+  {
+    name: string;
+    mint: string;
+    imageUri: string;
+    updateAuthority: string;
+    attributes: { trait_type: string; value: string }[];
+    tokenStandard: string;
+  }[]
+> {
+  console.log("Loading NFTs...");
+  const options = {
+    method: "GET",
+    headers: { accept: "application/json" },
+  };
 
-    const rawassets = await umi.rpc.getAssetsByOwner({ owner, limit: 10 });
-    const assets = rawassets;
-    //for loop logging out assets
-    for (let i = 0; i < assets.items.length; i++) {
-      console.log("ID: " + assets.items[i].id);
-    }
+  const response = await fetch(
+    "https://api-devnet.magiceden.dev/v2/wallets/" + wallet + "/tokens",
+    options
+  );
+  const data = await response.json();
+  const arrayResponse = Array.isArray(data) ? data : [];
+  const resultArray: {
+    name: string;
+    mint: string;
+    imageUri: string;
+    updateAuthority: string;
+    attributes: { trait_type: string; value: string }[];
+    tokenStandard: string;
+  }[] = [];
+  for (let i = 0; i < arrayResponse.length; i++) {
+    const item = {
+      name: arrayResponse[i].name,
+      mint: arrayResponse[i].mintAddress,
+      imageUri: arrayResponse[i].image,
+      updateAuthority: arrayResponse[i].updateAuthority,
+      attributes: arrayResponse[i].attributes,
+      tokenStandard: arrayResponse[i].tokenStandard || 0,
+    };
+    resultArray.push(item);
   }
+  console.log(resultArray);
+  return resultArray;
 }
