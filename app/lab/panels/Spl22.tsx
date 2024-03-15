0472d7d7f2bdd2e810bb035d7749981688bdb0d4
Batuhan Güçlü <85075133+deBatu@users.noreply.github.com>
2024-03-15 05:49:47 +0100

    Update Spl22.tsx

--- app/lab/panels/Spl22.tsx
+++ app/lab/panels/Spl22.tsx
@@ -1,63 +1,81 @@
-"use client";
-import { uploadFileToIrys, validateImage } from "@/backend/General";
+import React, { useState } from "react";
 import { AnimatePresence, motion as m } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { CustomSlider } from "@/components/ui/Slider";
+import { uploadFileToIrys, validateImage } from "@/backend/General";
 import { enqueueSnackbar } from "notistack";
-import { useState } from "react";
-import { createSPL22 } from "@/backend/SPL22";
+import { createToken22, burnToken22 } from "@/backend/Token22";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 export default function Panel() {
-  //sets the title of Token.
-  const [title, setTitle] = useState<string>();
-  //sets the symbol of Token.
-  const [symbol, setSymbol] = useState<string>();
-  //sets the description of Token.
-  const [description, setDescription] = useState<string>();
-  //sets the total supply of Token.
-  const [supply, setSupply] = useState<number>(0);
-  //sets the decimals of the Token.
-  const [decimals, setDecimal] = useState<number>(0);
-  //sets the image of Token.
-  const [image, setImage] = useState();
-  //sets the image-preview of Token.
-  const [imagePreview, setImagePreview] = useState();
-  //token extensions
-  const [frozen, setFrozen] = useState<boolean>(false);
-  const [transferTax, setTransferTax] = useState<number>(0);
-  const [interest, setInterest] = useState<number>(0);
-  const [authority, setAuthority] = useState<string>();
-
   const { wallet } = useWallet();
   const { connection } = useConnection();
+
+  const [attributeModal, setAttributeModal] = useState(false);
+  //rerenders the attribute-modal on every change.
+  const [renderHook, setRenderHook] = useState<number>(0);
+  //sets the title of NFT.
+  const [title, setTitle] = useState<string>();
+  //sets the symbol of NFT.
+  const [symbol, setSymbol] = useState<string>();
+  //sets the description of NFT.
+  const [description, setDescription] = useState<string>();
+  //sets the image of NFT.
+  const [image, setImage] = useState<File>();
+  //sets the image-preview of NFT.
+  const [imagePreview, setImagePreview] = useState<string>();
+  //sets the title of NFT.
+  const [sliderValue, setSliderValue] = useState<number>(0);
+  // hooks to store the key and value of the attribute to be added.
+  const [key, setKey] = useState<string>();
+  const [value, setValue] = useState<string>();
+  // a hook with the type of an array of objects, which contains the key and value of the attribute.
+  const [attributes, setAttributes] = useState<{ trait_type: string; value: string }[]>();
+  //hooks to store the key and value of the attribute to be added.
+  const removeAttribute = (index: number) => {
+    const oldArray = attributes;
+    if (oldArray) {
+      oldArray.splice(index, 1);
+      console.log(oldArray);
+    }
+  };
+
   const run = async () => {
     if (!wallet || !connection || !title || !symbol || !description || !image) {
       enqueueSnackbar("Fill out the empty fields.", {
         variant: "error",
       });
     } else {
-      console.log("Creating the SPL22.");
-
       enqueueSnackbar("Uploading image...", { variant: "info" });
       const imageUri = await uploadFileToIrys({
         wallet: wallet,
         connection: connection,
         file: image,
       });
+      
+      const rawData = {
+        seller_fee_basis_points: sliderValue,
+      };
 
-      const res = await createSPL22({
-        wallet: wallet,
-        connection: connection,
-        name: title,
-        symbol: symbol,
-        decimals: 0,
-        uri: imageUri,
-      });
+      if (imageUri) {
+        const tokenMintAddress = await createToken22({
+          wallet,
+          connection: connection,
+          name: title,
+          symbol: symbol,
+          decimals: 9, // Set the number of decimals
+          uri: imageUri, // Use the image URL as the token's URI
+          rawData,
+        });
 
-      if (res) {
-        enqueueSnackbar("Token created.", { variant: "success" });
-        console.log(res);
+        if (tokenMintAddress) {
+          enqueueSnackbar("Token created!", { variant: "success" });
+        } else {
+          enqueueSnackbar("Error creating token.", { variant: "error" });
+        }
       } else {
-        enqueueSnackbar("Error creating token.", { variant: "error" });
+        enqueueSnackbar("Image upload failed.", { variant: "error" });
       }
     }
   };
