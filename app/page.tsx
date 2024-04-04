721d79a85408cd9b5211da11232366a32057842a
M2DT <mtcreatives@hotmail.com>
2024-04-04 15:58:05 +0200

    theres always something to lint, innit

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
 "use client";
-
-import Header from "@/components/Header";
-import Navigation from "@/components/Navigation";
-import { useState } from "react";
-
-//panels
+import Container from "@/components/container/Container";
+import React from "react";
 import Panel from "../components/panels/NFT_Create";
-export default function Home() {
-  //hooks
-  const [theme, setTheme] = useState(0);
-  const id = 0;
 
-  return (
-    <div
-      id="skeleton"
-      className="skeleton"
-      data-theme={theme == 0 ? "light" : "dark"}
-    >
-      <Header id={id} theme={theme} />
-      <div id="mainboard" className="flex-row-center-start">
-        <Panel />
-      </div>
-      <Navigation
-        theme={theme}
-        toggleTheme={() => setTheme(theme == 0 ? 1 : 0)}
-      />
-    </div>
-  );
+export default function page() {
+  return <Container id={0} panel={<Panel />} />;
 }
