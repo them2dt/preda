3e7b7f47a0a6f8ad5a39f86527df88ecb2b18fb5
M2DT <mtcreatives@hotmail.com>
2024-04-08 17:50:30 +0200

    lint

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,25 +7,18 @@ import { fetchAsset } from "@/backend/CORE";
 import { Connection } from "@solana/web3.js";
 //Container to wrap all elements of a page into a container.
 
-export default function Container({
-  id,
-  panel,
-}: {
-  id: number;
-  panel: React.JSX.Element;
-}) {
+export default function Page() {
   const themes = ["light", "dark", "candy", "navy"];
   const [theme, setTheme] = useState(0);
   const { wallet } = useWallet();
   const { connection } = useConnection();
 
   const run = async () => {
-    await fetchAsset({ wallet: wallet, connection: connection ,assetId:""});
+    await fetchAsset({ wallet: wallet, connection: connection, assetId: "" });
   };
   return (
     <div>
       <div id="skeleton" className="skeleton" data-theme={themes[theme]}>
-        <Header id={id} theme={theme} themes={themes} />
         <div id="mainboard" className="flex-row-center-start">
           <button onClick={run}>TEST</button>
         </div>
